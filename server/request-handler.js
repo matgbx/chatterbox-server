f6a8ef11402a1a909d03adaf0faef57640e193af
matgbx <matbagnall@gmail.com>
2018-02-05 16:43:22 -0800

    Implemented code to pass Server Spec

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,17 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+const testObj = {
+  absURL: '/classes/messages', 
+  results: [],
+};
+
+const defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -27,11 +38,14 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
+// **************************************
+  // set conditions that check if request is POST/GET/etc
+
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
   // The outgoing status.
   var statusCode = 200;
-
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
@@ -41,9 +55,55 @@ var requestHandler = function(request, response) {
   // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'text/plain';
 
+  
+  //check error first
+  // request.on('error', (err) => {
+  //   console.error(err);
+  //   statusCode = 400;
+  //   response.writeHead(statusCode, headers);
+  //   response.end();
+  // });
+  // If get
+  if (testObj.absURL.includes(request.url)) {
+    if (request.method === 'GET') {
+      //   check for errors GET related
+      //   give write head with code 200 and headers
+      response.writeHead(statusCode, headers);
+      //   end with JSON String of obj
+      console.log(testObj);
+      response.end(JSON.stringify(testObj));
+    } else if (request.method === 'POST') {
+      // else if post
+      // check for errors POST related
+      let body = [];
+      // chunk the data together
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        // concat the data
+        body = Buffer.concat(body).toString();
+        // push the parse into textObj.results
+        testObj.results.push(JSON.parse(body));
+        // console.log(testObj);
+        
+        // write head for code 201
+        statusCode = 201;
+        response.writeHead(statusCode, headers);
+        // call end
+        response.end();
+        console.log(testObj.results);
+      });  
+    }
+  } else {
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    // call end
+    response.end();
+  }
+  // console.log(testObj);
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +112,8 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // make end end a JSON string
+  // response.end(JSON.stringify(testObj));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +125,6 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
+// add export of the handle function
+exports.handleRequest = requestHandler;
